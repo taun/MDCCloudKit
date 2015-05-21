3b6ceab47c63fcf8220111144c58e134159f265f
Taun Chapman <tchapman@moedae.com>
2015-05-21 14:22:55 -0400

    Initial Commit

---
+++ MDCloudKit/MDCloudKit.h
@@ -0,0 +1,19 @@
+//
+//  MDCloudKit.h
+//  MDCloudKit
+//
+//  Created by Taun Chapman on 05/21/15.
+//  Copyright (c) 2015 MOEDAE LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MDCloudKit.
+FOUNDATION_EXPORT double MDCloudKitVersionNumber;
+
+//! Project version string for MDCloudKit.
+FOUNDATION_EXPORT const unsigned char MDCloudKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MDCloudKit/PublicHeader.h>
+
+
